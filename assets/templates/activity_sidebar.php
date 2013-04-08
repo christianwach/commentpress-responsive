047254eed7e9a332485f08fd33f144a25d16661a
Christian Wach <needle@haystack.co.uk>
2013-04-08 11:57:30 +0100

    enable populate_extras on buddypress members in sidebar

--- assets/templates/activity_sidebar.php
+++ assets/templates/activity_sidebar.php
@@ -298,7 +298,7 @@ if (
 		'&type=active'.
 		'&per_page='.$_max_members.
 		'&max='.$_max_members.
-		'&populate_extras=0' 
+		'&populate_extras=1' 
 		
 	) ) : ?>
 	
@@ -317,30 +317,13 @@ if (
 				</div>
 	
 				<div class="item">
+
 					<div class="item-title">
 						<a href="<?php bp_member_permalink(); ?>"><?php bp_member_name(); ?></a>
-	
-						<?php if ( bp_get_member_latest_update() ) : ?>
-	
-							<span class="update"> <?php bp_member_latest_update(); ?></span>
-	
-						<?php endif; ?>
-	
 					</div>
 	
 					<div class="item-meta"><span class="activity"><?php bp_member_last_active(); ?></span></div>
-	
-					<?php do_action( 'bp_directory_members_item' ); ?>
-	
-					<?php
-					 /***
-					  * If you want to show specific profile fields here you can,
-					  * but it'll add an extra query for each member in the loop
-					  * (only one regardless of the number of fields you show):
-					  *
-					  * bp_member_profile_data( 'field=the field name' );
-					  */
-					?>
+					
 				</div>
 	
 				<div class="clear"></div>
@@ -366,7 +349,7 @@ if (
 		'&type=online'.
 		'&per_page='.$_max_members.
 		'&max='.$_max_members.
-		'&populate_extras=0' 
+		'&populate_extras=1' 
 		
 	) ) : ?>
 	
@@ -385,30 +368,13 @@ if (
 				</div>
 	
 				<div class="item">
+
 					<div class="item-title">
 						<a href="<?php bp_member_permalink(); ?>"><?php bp_member_name(); ?></a>
-	
-						<?php if ( bp_get_member_latest_update() ) : ?>
-	
-							<span class="update"> <?php bp_member_latest_update(); ?></span>
-	
-						<?php endif; ?>
-	
 					</div>
 	
 					<div class="item-meta"><span class="activity"><?php bp_member_last_active(); ?></span></div>
 	
-					<?php do_action( 'bp_directory_members_item' ); ?>
-	
-					<?php
-					 /***
-					  * If you want to show specific profile fields here you can,
-					  * but it'll add an extra query for each member in the loop
-					  * (only one regardless of the number of fields you show):
-					  *
-					  * bp_member_profile_data( 'field=the field name' );
-					  */
-					?>
 				</div>
 	
 				<div class="clear"></div>
